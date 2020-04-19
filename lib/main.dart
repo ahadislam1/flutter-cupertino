eebfba14d1f3c61b2912971642fcf08c5c64144f
Ahad Islam <ahad.islam624@gmail.com>
2020-04-19 15:10:31 -0400

    edited pubspec and added app.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/services.dart';
 
-// import 'app.dart';
+import 'app.dart';
 
 void main() {
   SystemChrome.setPreferredOrientations(
